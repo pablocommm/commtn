cbe07010f343b51308843758375efd2c5bca61ed
Pablo <pablo@commm.co>
2019-02-13 15:38:50 -0500

    hide add and máx size slider

--- front-page.php
+++ front-page.php
@@ -22,7 +22,6 @@ get_header(); ?>
 
         <div class="col s12 no-padding">
           <?php do_action('featured_feed') ?>
-          <!-- <?php do_action('TN_HOM'); ?> -->
         </div>
         
 
