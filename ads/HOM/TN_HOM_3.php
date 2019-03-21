3a94e9898baff2e7d29d3683d9e3420376bb27f0
Pablo <pablo@commm.co>
2019-03-20 20:13:57 -0500

    ad class for last ad

--- ads/HOM/TN_HOM_3.php
+++ ads/HOM/TN_HOM_3.php
@@ -1,6 +1,6 @@
 <div class="ad-container">
 <!-- /21751214561/CMN_300x250_2 -->
-<div id='div-gpt-ad-1553130180497-0' style='height:600px; width:300px;'>
+<div id='div-gpt-ad-1553130180497-0' class="ad" style='height:600px; width:300px;'>
 <script>
 googletag.cmd.push(function() { googletag.display('div-gpt-ad-1553130180497-0'); });
 </script>
