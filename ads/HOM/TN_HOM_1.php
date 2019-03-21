52429dc94033758eb3cd87d7fac6583958966e57
Pablo <pablo@commm.co>
2019-03-20 19:24:13 -0500

    ad first home priority hight

--- ads/HOM/TN_HOM_1.php
+++ ads/HOM/TN_HOM_1.php
@@ -1,6 +1,8 @@
 <!-- /21751214561/tnhom1 -->
-<div id='div-gpt-ad-1553127408561-0' style='height:250px; width:300px;'>
+<div class="ad-container">
+<div id='div-gpt-ad-1553127408561-0'  class="ad" style='height:250px; width:300px;'>
 <script>
 googletag.cmd.push(function() { googletag.display('div-gpt-ad-1553127408561-0'); });
 </script>
+</div>
 </div>
