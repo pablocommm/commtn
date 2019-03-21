5fdacf5a7aa43bc2ba0b219f89dd860a8a29523a
Pablo <pablo@commm.co>
2019-03-20 19:38:16 -0500

    ads try

--- inc/first_side_feed.php
+++ inc/first_side_feed.php
@@ -108,4 +108,37 @@ wp_reset_query();
 
 </div>
 
+<div class="col s12">
+<?php 
+            if ( is_front_page() && is_home() )  {
+              // Default HOM
+              do_action('TN_HOM_3');
+            } elseif ( in_category( '2006' )) {
+              // TV
+              echo'';
+            } elseif ( in_category( '25' )) {
+              // ENTRETENIMIENTO
+              echo'';
+            } elseif ( in_category( '4' )) {
+              // VIRAL
+              echo'';
+            } elseif ( in_category( '44' )) {
+              // MODA
+              echo'';
+            } elseif ( in_category( '4700' )) {
+              // YOUTUBERS
+              echo'';
+            } elseif ( in_category( '4615' )) {
+              // LO MEJOR Y LO PEOR
+              echo'';
+            } elseif ( in_category( '14' )) {
+              // DEPORTES
+              echo'';
+            } else {
+              //Síguenos
+              echo'';
+            }
+?>
+
+</div>
 
