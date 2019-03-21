691722f6196c67d5673b733abc414d67de837669
Pablo <pablo@commm.co>
2019-03-21 15:15:17 -0500

    change ad in hom-1

--- ads/HOM/TN_HOM_1.php
+++ ads/HOM/TN_HOM_1.php
@@ -1,8 +1,6 @@
 <!-- /21751214561/tnhom1 -->
-<div class="ad-container">
-<div id='div-gpt-ad-1553127408561-0'  class="ad" style='height:250px; width:300px;'>
+<div id='div-gpt-ad-1553197024783-0' class='ad' style='height:250px; width:300px;'>
 <script>
-googletag.cmd.push(function() { googletag.display('div-gpt-ad-1553127408561-0'); });
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1553197024783-0'); });
 </script>
-</div>
 </div>
