3c3d5d2772e9501d6e956cf51e1a484e7d5020d9
Pablo <pablo@commm.co>
2019-04-23 12:12:42 -0500

    ad insert try

--- inc/first_side_feed.php
+++ inc/first_side_feed.php
@@ -67,6 +67,12 @@ wp_reset_query();
 
 <div class="col s12">
 
+<?php echo adinserter (1); ?>
+
+</div>
+
+<div class="col s12">
+
 <ul class="collection no-border">
     <li class="collection-item grey darken-4 white-text">
     Últimas noticias
@@ -110,10 +116,6 @@ wp_reset_query();
 
 
 
-<div class="col s12">
 
-<?php echo adinserter (1); ?>
-
-</div>
 
 
