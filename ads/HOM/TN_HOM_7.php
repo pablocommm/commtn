f213d36be60521a651ab4444ec63e1c99af80917
Pablo <pablo@commm.co>
2019-04-23 10:55:27 -0500

    728 x 90 show

--- ads/HOM/TN_HOM_7.php
+++ ads/HOM/TN_HOM_7.php
@@ -1,7 +1,7 @@
-<div class="ad-container hide">
+<div class="ad-container">
 
 <!-- /21751214561/tnsig2 -->
-<div id='div-gpt-ad-1555004420739-0' class="ad hide" style='height:90px; width:728px;'>
+<div id='div-gpt-ad-1555004420739-0' class="ad" style='height:90px; width:728px;'>
 <script>
 googletag.cmd.push(function() { googletag.display('div-gpt-ad-1555004420739-0'); });
 </script>
