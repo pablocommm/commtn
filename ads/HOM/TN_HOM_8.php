1422cc24a7ee943e1de7d5b9f4fb084b2b1e9c59
Pablo <pablo@commm.co>
2019-04-23 10:28:02 -0500

    hide movile single page ad

--- ads/HOM/TN_HOM_8.php
+++ ads/HOM/TN_HOM_8.php
@@ -1,5 +1,5 @@
 <!-- /21751214561/tnytb1 -->
-<div id='div-gpt-ad-1555080108446-0' class="ad hide-on-med-and-up" style='height:250px; width:300px;'>
+<div id='div-gpt-ad-1555080108446-0' class="ad hide" style='height:250px; width:300px;'>
 <script>
 googletag.cmd.push(function() { googletag.display('div-gpt-ad-1555080108446-0'); });
 </script>
